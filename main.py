8bc32fbb5a2161f59270067b71ee4df90c8cfba5
w3st1e <Nikita.bebikov@gmail.com>
2025-07-14 13:58:39 -0600

    Added russian language, settings button and more.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import asyncio
-import requests
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
@@ -8,7 +7,6 @@ from usersdb import db
 from weather.getweather import get_weather
 from aiogram.fsm.state import State, StatesGroup
 from apikeys.get_apis import get_api
-from aiogram.fsm.context import FSMContext
 
 dp = Dispatcher()
 
@@ -19,27 +17,86 @@ class City(StatesGroup):
 
 @dp.message(Command('start'))
 async def cmd_start(message: types.Message):
-    kb = [[InlineKeyboardButton(text='Get weather', callback_data='weather')], [InlineKeyboardButton(text='Settings', callback_data='settings'), InlineKeyboardButton(text='My balance', callback_data='balance')]]
-    await message.answer(f"Hello, {message.from_user.full_name}! My name is WeatherBot⛅\nHow can I help you?🙂 /help", reply_markup=types.InlineKeyboardMarkup(inline_keyboard=kb))
-    await db.add_to_db(message.from_user.id)
+    en_kb = [[InlineKeyboardButton(text='Get weather', callback_data='weather')], [InlineKeyboardButton(text='Settings', callback_data='settings'), InlineKeyboardButton(text='My balance', callback_data='balance')]]
+    ru_kb = [[InlineKeyboardButton(text='Получить погоду', callback_data='weather')], [InlineKeyboardButton(text='Настройки', callback_data='settings'), InlineKeyboardButton(text='Мой баланс', callback_data='balance')]]
+    kb = en_kb if await db.get_lang(message.from_user.id) == 'en' else ru_kb
+    if await db.get_lang(message.from_user.id) =='en':
+        await message.answer(f"Hello, {message.from_user.full_name}! My name is WeatherBot⛅\nHow can I help you?🙂 /help", reply_markup=types.InlineKeyboardMarkup(inline_keyboard=kb))
+    else:
+        await message.answer(f"Привет, {message.from_user.full_name}! Меня зовут WeatherBot⛅\nКак я могу помочь вам?🙂 /help", reply_markup=types.InlineKeyboardMarkup(inline_keyboard=kb))
+    await db.add_to_db(message.from_user.id, balance=5)
 
 @dp.message(Command('help'))
 async def cmd_help(message: types.Message):
     pass
 
+
+@dp.message(Command('lang'))
+async def cmd_lang(message: types.Message):
+    if await db.get_lang(message.from_user.id) == 'en':
+        await db.set_lang(message.from_user.id, 'ru')
+        await message.answer('Язык успешно изменен на русский')
+    else:
+        await db.set_lang(message.from_user.id, 'en')
+    await message.answer('Language updated successfully')
 @dp.callback_query(F.data == 'weather')
 async def weather(callback: types.CallbackQuery, state: FSMContext):
-    await callback.message.answer('Enter your city name')
-    await state.set_state(City.city)
+    if await db.get_balance(callback.from_user.id) <= 0:
+        if await db.get_lang(callback.from_user.id) == 'ru':
+            await callback.message.answer('У вас недостаточно вызовов для получения погоды. Пополните баланс.')
+        else:
+            await callback.message.answer("You don't have enough calls to get the weather. Please recharge your balance.")
+    else:
+        if await db.get_lang(callback.from_user.id) == 'ru':
+            await callback.message.answer('Введите название вашего города')
+        else:
+            await callback.message.answer('Enter your city name')
+        await state.set_state(City.city)
+
+@dp.callback_query(F.data == 'balance')
+async def get_balance(callback: types.CallbackQuery):
+    user_id = callback.from_user.id
+    balance = await db.get_balance(user_id)
+    if await db.get_lang(user_id) == 'ru':
+        await callback.message.answer(f'Ваш баланс: {balance} вызовов')
+    await callback.message.answer(f'Your balance: {balance} calls')
 
 
 @dp.message(City.city)
 async def get_city_name(message: types.Message, state: FSMContext):
-    await state.update_data(city = message)
+    await state.update_data(city=message)
     data = await state.get_data()
     weather_data = await get_weather(data['city'].text, get_api()[0])
-    await message.answer(f'🏙️ Weather for {weather_data['name']} City:️\n    🌡️ Temperature:️ current {weather_data['main']['temp']}, feels like {weather_data['main']['feels_like']}\n    ⛅ {weather_data['weather'][0]['description'].capitalize()}\n  💨 Wind speed: {weather_data['wind']['speed']}')
+    if await db.get_lang(message.from_user.id) == 'ru':
+        await message.answer(f'🏙️ Погода для города {weather_data["name"]}:\n    🌡️ Температура: текущая {weather_data["main"]["temp"]}, ощущается как {weather_data["main"]["feels_like"]}\n    ⛅ {weather_data["weather"][0]["description"].capitalize()}\n  💨 Скорость ветра: {weather_data["wind"]["speed"]}')
+    else:
+        await message.answer(f'🏙️ Weather for {weather_data['name']} City:️\n    🌡️ Temperature:️ current {weather_data['main']['temp']}, feels like {weather_data['main']['feels_like']}\n    ⛅ {weather_data['weather'][0]['description'].capitalize()}\n  💨 Wind speed: {weather_data['wind']['speed']}')
+    await db.change_balance(message.from_user.id, await db.get_balance(message.from_user.id) - 1)
     await state.clear()
+
+@dp.callback_query(F.data == 'settings')
+async def settings(callback: types.CallbackQuery):
+    if await db.get_lang(callback.from_user.id) == 'ru':
+        await callback.message.answer('Выберите действие:', reply_markup=types.InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Изменить язык', callback_data='change_language')], [InlineKeyboardButton(text='Пополнить баланс', callback_data='recharge_balance')]]))
+    else:
+        await callback.message.answer('Choose an action:', reply_markup=types.InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Change language', callback_data='change_language')], [InlineKeyboardButton(text='Recharge balance', callback_data='recharge_balance')]]))
+
+
+@dp.callback_query(F.data == 'change_language')
+async def change_language(callback: types.CallbackQuery):
+    if await db.get_lang(callback.from_user.id) == 'ru':
+        await db.set_lang(callback.from_user.id, 'en')
+        await callback.message.answer('Language changed to English successfully')
+    else:
+        await db.set_lang(callback.from_user.id, 'ru')
+        await callback.message.answer('Язык успешно изменен на русский')
+@dp.callback_query(F.data == 'recharge_balance')
+async def recharge_balance(callback: types.CallbackQuery):
+    await db.change_balance(callback.from_user.id, 5)
+    if await db.get_lang(callback.from_user.id) == 'ru':
+        await callback.message.answer('Ваш баланс успешно пополнен на 5 вызовов.')
+    else:
+        await callback.message.answer('Your balance has been successfully recharged with 5 calls.')
 
 
 async def main() -> None:
--- usersdb/db.py
+++ usersdb/db.py
@@ -10,3 +10,39 @@ async def add_to_db(user_id, balance=0, lang='en'):
         await connect.commit()
     await cursor.close()
     await connect.close()
+
+
+async def get_balance(user_id):
+    connect = await aiosqlite.connect('D:/pythoncodes/TelegramBotWeather/weathertg.db')
+    cursor = await connect.cursor()
+    await cursor.execute('SELECT balance FROM users WHERE userid = ?', (user_id,))
+    balance = await cursor.fetchone()
+    await cursor.close()
+    await connect.close()
+    return balance[0] if balance else 0
+
+async def get_lang(user_id):
+    connect = await aiosqlite.connect('D:/pythoncodes/TelegramBotWeather/weathertg.db')
+    cursor = await connect.cursor()
+    await cursor.execute('SELECT lang FROM users WHERE userid = ?', (user_id,))
+    lang = await cursor.fetchone()
+    await cursor.close()
+    await connect.close()
+    return lang[0] if lang else 'en'
+
+async def set_lang(user_id, language) -> None:
+    connect = await aiosqlite.connect('D:/pythoncodes/TelegramBotWeather/weathertg.db')
+    cursor = await connect.cursor()
+    await cursor.execute('UPDATE users SET lang = ? WHERE userid = ?', (language, user_id))
+    await cursor.close()
+    await connect.close()
+
+async def change_balance(user_id, calls) -> None:
+    connect = await aiosqlite.connect('D:/pythoncodes/TelegramBotWeather/weathertg.db')
+    cursor = await connect.cursor()
+    await cursor.execute('UPDATE users SET balance = ? WHERE userid = ?', (calls, user_id))
+    await connect.commit()
+    await cursor.close()
+    await connect.close()
+
+
